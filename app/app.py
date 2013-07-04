d4a3c9268a8431f11ea17dd9ac73783017038314
Michael Lawrence Evans <michael@codeforamerica.org>
2013-07-04 15:44:32 -0500

    Adding basic Flask app

---
+++ app/app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def dashboard():
+    """
+        Render the main dashboard view.
+    """
+    
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run()
