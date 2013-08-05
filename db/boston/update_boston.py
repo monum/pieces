f901e86492d53086c4d3510198bcfef15acedaad
Michael Lawrence Evans <michael@codeforamerica.org>
2013-08-05 16:47:46 -0500

    Fixing formatting

--- db/boston/update_boston.py
+++ db/boston/update_boston.py
@@ -163,8 +163,8 @@ if __name__ == '__main__':
 
         start, end = compute_time_range(end_date, 1) # Just handling one day at a time
 
-	latest_response_length = None 
-	
+        latest_response_length = None 
+    
         for day in xrange(num_of_days):
             page = 1
             more_pages = True  
